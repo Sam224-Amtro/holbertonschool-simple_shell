f68c14b201ecc35d9d4d5e268a42e19470a952e4
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-08-15 11:39:12 +0200

    Here is : main.c, parser.c, exercute.c, main.h and utils.c

--- main.h
+++ main.h
@@ -14,18 +14,21 @@ extern char **environ;
 /* main.c */
 int main(int argc, char **argv);
 void shell_loop(void);
-void remove_trailing_newline(char *line);
 
 /* parser.c */
 char *read_line(void);
 char **parse_line(char *line);
-int count_tokens(char *line);
 
 /* execute.c */
 int execute_command(char **args);
+void test_env(char **env);
+void search_path(int ac, char **av);
+int main(int ac, char **av, char **env);
+
 
 /* utils.c */
 void free_args(char **args);
 void print_error(char *prog_name, char *cmd);
+void remove_trailing_newline(char *str);
 
 #endif
