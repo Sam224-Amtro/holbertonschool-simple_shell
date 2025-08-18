4dcedfea11c9b00bd98cf69c0fda3b2d0321ec4f
Samamtro <fofanasanoussy545@gmail.com>
2025-08-19 01:03:41 +0200

    the file to my main

--- main.c
+++ main.c
@@ -6,68 +6,89 @@
 #include <sys/wait.h>
 
 /**
- * main - simple shell
+ * read_line - Lit une ligne depuis l'entrée standard
  *
- * Return: Always 0
+ * Return: Pointeur vers la ligne lue (doit être free par l'appelant)
+ */
+char *read_line(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+
+	printf("$ ");
+	nread = getline(&line, &len, stdin);
+	if (nread == -1)
+	{
+		free(line);
+		printf("\n");
+		exit(EXIT_SUCCESS);
+	}
+
+	if (line[nread - 1] == '\n')
+		line[nread - 1] = '\0';
+
+	return (line);
+}
+
+/**
+ * execute_command - Exécute une commande simple
+ * @line: La commande à exécuter
+ */
+void execute_command(char *line)
+{
+	pid_t pid;
+	char *args[2];
+	int status;
+
+	if (strcmp(line, "exit") == 0)
+	{
+		free(line);
+		exit(EXIT_SUCCESS);
+	}
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		free(line);
+		exit(EXIT_FAILURE);
+	}
+
+	if (pid == 0)
+	{
+		args[0] = line;
+		args[1] = NULL;
+
+		if (execvp(args[0], args) == -1)
+		{
+			perror("execvp");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		wait(&status);
+	}
+}
+
+/**
+ * main - Point d'entrée de la simple_shell
+ *
+ * Return: Toujours 0
  */
 int main(void)
 {
 	char *line;
-	size_t len;
-	ssize_t nread;
-
-	line = NULL;
-	len = 0;
 
 	while (1)
 	{
-		pid_t pid;
-		char *args[2];
-		int status;
+		line = read_line();
 
-		printf("$ ");
-		nread = getline(&line, &len, stdin);
-		if (nread == -1)
-		{
-			free(line);
-			printf("\n");
-			exit(EXIT_SUCCESS);
-		}
+		execute_command(line);
 
-		if (line[nread - 1] == '\n')
-			line[nread - 1] = '\0';
-
-		if (strcmp(line, "exit") == 0)
-		{
-			free(line);
-			exit(EXIT_SUCCESS);
-		}
-
-		pid = fork();
-		if (pid == -1)
-		{
-			perror("fork");
-			free(line);
-			exit(EXIT_FAILURE);
-		}
-
-		if (pid == 0)
-		{
-			args[0] = line;
-			args[1] = NULL;
-
-			if (execvp(args[0], args) == -1)
-			{
-				perror("execvp");
-				exit(EXIT_FAILURE);
-			}
-		}
-		else
-		{
-			wait(&status);
-		}
+		free(line);
 	}
 
-	free(line);
 	return (0);
 }
