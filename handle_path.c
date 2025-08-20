adf9a9416346af222ae6a283ffd1419333fc2c54
Samamtro <fofanasanoussy545@gmail.com>
2025-08-21 00:21:50 +0200

    the file to my handle path

--- handle_path.c
+++ handle_path.c
@@ -1,20 +1,23 @@
 #include "main.h"
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
 /**
  * find_full_path - Obtient le chemin absolu d'une commande
  * @command: nom de la commande (ex: "ls")
  * @envp: tableau des variables d'environnement
- *
- * Si @command contient un '/', il est considéré comme un chemin
- * absolu ou relatif.
- * Sinon, la fonction parcourt les dossiers listés dans la variable PATH et
- * renvoie le premier chemin où la commande est exécutable.
  *
  * Return: chaîne allouée contenant le chemin complet si trouvé, NULL sinon.
  */
 char *find_full_path(char *command, char **envp)
 {
 	char *path, *path_copy, *token, *full_path;
-	int len;
+	size_t len;
+
+	if (!command || !*command)
+		return (NULL);
 
 	if (strchr(command, '/'))
 		return (strdup(command));
@@ -37,8 +40,7 @@ char *find_full_path(char *command, char **envp)
 			free(path_copy);
 			return (NULL);
 		}
-		sprintf(full_path, "%s/%s", token, command);
-
+		snprintf(full_path, len, "%s/%s", token, command);
 		if (access(full_path, X_OK) == 0)
 		{
 			free(path_copy);
