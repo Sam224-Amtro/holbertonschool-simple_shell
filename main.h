1b7e4b63fa4c12871b480853f4d9f82992631ebf
Samamtro <fofanasanoussy545@gmail.com>
2025-08-20 15:49:28 +0200

    the file to main

--- main.h
+++ main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,18 +8,15 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-
-char **_realloc_args(char **args, int old_size, int new_size);
-char *get_path_from_env(char **envp);
-
-
-void remove_trailing_newline(char *line);
 void shell_loop(char **argv, char **envp);
-int main(int argc, char **argv, char **envp);
+void remove_trailing_newline(char *line);
 char **parse_line(char *line);
-char *find_full_path(char *command, char **envp);
-int execute_command(char *command, char **args, char **envp);
+char **_realloc_args(char **args, int old_size, int new_size);
 int handle_builtin(char **args, char **envp, char *line);
+void free_args(char **args);
 
+int execute_command(char *cmd, char **args, char **envp);
+char *find_full_path(char *command, char **envp);
+char *get_path_from_env(char **envp);
 
 #endif /* MAIN_H */
