599a1b844d1a52021900481a2fba70f15b30c011
Samamtro <fofanasanoussy545@gmail.com>
2025-08-21 01:43:50 +0200

    the file to my builtin

--- builtins.c
+++ builtins.c
@@ -3,11 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <stdlib.h>
-
 /**
  * free_args - libère correctement le tableau d'arguments
  * @args: tableau d'arguments à libérer
+ *
+ * NB: si args[i] vient de strtok(line), il ne faut PAS faire free(args[i])
  */
 void free_args(char **args)
 {
@@ -16,24 +16,21 @@ void free_args(char **args)
 }
 
 /**
- * handle_builtin - exécute les commandes internes du shell
+ * handle_builtin - Exécute les commandes internes du shell
  * @args: tableau contenant la commande et ses arguments
  * @envp: tableau des variables d'environnement
  * @line: ligne saisie par l'utilisateur (libérée si nécessaire)
  *
- * Return: 1 si une built-in a été exécutée, 0 sinon
+ * Return: 1 si une commande interne a été exécutée, 0 sinon
  */
 int handle_builtin(char **args, char **envp, char *line)
 {
 	int i;
 
-	if (args == NULL && args[0] == NULL)
-		return (0);
-
 	if (strcmp(args[0], "exit") == 0)
 	{
+		free_args(args);
 		free(line);
-		free(args);
 		exit(0);
 	}
 
