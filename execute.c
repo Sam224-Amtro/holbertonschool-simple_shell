e878c8b6014e46afcceb30457c319473d702eda4
Samamtro <fofanasanoussy545@gmail.com>
2025-08-21 12:14:25 +0200

    the file to my execute

--- execute.c
+++ execute.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
 /**
  * execute_command - Crée un processus pour exécuter une commande
@@ -13,16 +14,17 @@
  * Return: Code de sortie du fils si terminé normalement
  *          127 si execve échoue,
  *         -1 si le processus s'est terminé anormalement.
- * Description :
- * Cette fonction crée un processus fils pour exécuter une commande
- * en utilisant execve. Le processus parent attend que le fils termine.
- * Si execve échoue dans le fils, celui-ci quitte avec le code 127
- * (convention Unix pour "commande non trouvée").
  */
 int execute_command(char *command, char **args, char **envp)
 {
 	int status;
 	pid_t pid;
+
+	if (command == NULL || args == NULL || envp == NULL)
+	{
+		fprintf(stderr, "Invalid arguments to execute_command\n");
+		return (-1);
+	}
 
 	pid = fork();
 	if (pid == -1)
@@ -37,18 +39,17 @@ int execute_command(char *command, char **args, char **envp)
 		perror("execve");
 		exit(127);
 	}
-	else
+
+	if (waitpid(pid, &status, 0) == -1)
 	{
-		if (waitpid(pid, &status, 0) == -1)
-		{
-			perror("waitpid");
-			return (-1);
-		}
-		if (WIFEXITED(status))
-			return (WEXITSTATUS(status));
-		else if (WIFSIGNALED(status))
-			return (128 + WTERMSIG(status));
-		else
-			return (-1);
+		perror("waitpid");
+		return (-1);
 	}
+
+	if (WIFEXITED(status))
+		return (WEXITSTATUS(status));
+	else if (WIFSIGNALED(status))
+		return (128 + WTERMSIG(status));
+	else
+		return (-1);
 }
