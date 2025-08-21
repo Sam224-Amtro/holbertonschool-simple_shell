90293553b506182ac394e6b78b44ed2635e7e913
Samamtro <fofanasanoussy545@gmail.com>
2025-08-21 12:21:30 +0200

    the file to my utils

--- utils.c
+++ utils.c
@@ -1,20 +1,16 @@
 #include "main.h"
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * get_path_from_env - Récupère la valeur de la variable PATH depuis envp
  * @envp: tableau des variables d'environnement
- *
- * Description :
- * Parcourt envp (tableau de chaînes "NOM=VALEUR") à la recherche de "PATH=".
- * Si trouvée, retourne pointeur vers le début de la valeur (après "PATH=").
- * Sinon, retourne NULL.
  *
  * Return: pointeur vers la valeur de PATH (ne pas free!), ou NULL si absente
  */
 char *get_path_from_env(char **envp)
 {
 	int i;
-
 
 	for (i = 0; envp[i] != NULL; i++)
 	{
@@ -30,11 +26,6 @@ char *get_path_from_env(char **envp)
  * @old_size: nombre actuel d'éléments utilisés
  * @new_size: nouvelle capacité souhaitée
  *
- * Description :
- * Crée un nouveau tableau avec plus de place,
- * copie les anciens pointeurs dedans, libère l'ancien tableau,
- * et retourne le nouveau.
- *
  * Return: pointeur vers le nouveau tableau, ou NULL si malloc échoue
  */
 char **_realloc_args(char **args, int old_size, int new_size)
@@ -46,9 +37,14 @@ char **_realloc_args(char **args, int old_size, int new_size)
 	if (!new_args)
 		return (NULL);
 
-	for (i = 0; i < old_size; i++)
-		new_args[i] = args[i];
+	if (args != NULL)
+	{
+		for (i = 0; i < old_size; i++)
+			new_args[i] = args[i];
+		free(args);
+	}
+	for (i = old_size; i < new_size; i++)
+		new_args[i] = NULL;
 
-	free(args);
 	return (new_args);
 }
